e386f784c441200db1c96e8006e08b23405f1664
Zakaria haja <zakaria.figuig@gmail.com>
2020-09-16 02:50:51 +0100

    Updates

---
+++ precache-manifest.12c257be33915d3dc0edbf3b967ac605.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a9a928998135fcc93e775f521abc95e",
+    "url": "/lwachoun-app/index.html"
+  },
+  {
+    "revision": "72efe2cb567f6483175e",
+    "url": "/lwachoun-app/static/css/main.41f17742.chunk.css"
+  },
+  {
+    "revision": "df17cbd4355c0b876d6a",
+    "url": "/lwachoun-app/static/js/2.891dee9b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lwachoun-app/static/js/2.891dee9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72efe2cb567f6483175e",
+    "url": "/lwachoun-app/static/js/main.d9c71d82.chunk.js"
+  },
+  {
+    "revision": "41b7f8a8aff75c27feee",
+    "url": "/lwachoun-app/static/js/runtime-main.55067295.js"
+  },
+  {
+    "revision": "f1af54fc7d51cc519266d057befb81ab",
+    "url": "/lwachoun-app/static/media/BigelowRules-Regular.f1af54fc.ttf"
+  }
+]);
